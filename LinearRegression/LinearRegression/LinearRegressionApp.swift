af0189052a29d7499454776e21c39cfaa36c2f31
Avi Tsadok <avi.tsadok@melio.com>
2024-12-28 20:30:07 +0200

    First commit

---
+++ LinearRegression/LinearRegression/LinearRegressionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinearRegressionApp.swift
+//  LinearRegression
+//
+//  Created by Avi Tsadok on 28/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct LinearRegressionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
